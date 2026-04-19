8e25d24415768236129e49c2eb84feb76d03dd0a
Vlad Gheorghiu <vlad@192.168.1.132>
2026-04-19 22:48:26 +0300

    Added skeleton for AI NPC game

---
+++ src/ai_npc_game/ai_npc_game.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "components/simple_scene.h"
+
+
+namespace ai_npc
+{
+    class Game : public gfxc::SimpleScene
+    {
+     public:
+        Game();
+        ~Game();
+
+        void Init() override;
+
+     private:
+        void FrameStart() override;
+        void Update(float deltaTimeSeconds) override;
+        void FrameEnd() override;
+
+        void OnInputUpdate(float deltaTime, int mods) override;
+        void OnKeyPress(int key, int mods) override;
+        void OnKeyRelease(int key, int mods) override;
+        void OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY) override;
+        void OnMouseBtnPress(int mouseX, int mouseY, int button, int mods) override;
+        void OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods) override;
+        void OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY) override;
+        void OnWindowResize(int width, int height) override;
+
+        // Class variables
+        glm::vec3 clear_color;
+        std::vector<std::string> mesh_choices;
+        size_t mesh_index;
+        glm::vec3 mesh_pos;
+    };
+}   // namespace ai_npc
